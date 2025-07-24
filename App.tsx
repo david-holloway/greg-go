9f1fae0e4638fef495bb71707b23307d88ba2030
david-holloway <hollowaydavid22@gmail.com>
2025-07-24 16:49:09 -0400

    Imported SettingScreen and Greglist into App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import NameScreen from './src/screens/NameScreen';
 import PhotoScreen from './src/screens/PhotoScreen';
+import SettingScreen from './src/screens/SettingScreen';
+import GregListScreen from './src/screens/GregListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +16,8 @@ export default function App() {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Name" component={NameScreen} />
         <Stack.Screen name="Photo" component={PhotoScreen} />
+        <Stack.Screen name="Setting" component={SettingScreen} />
+        <Stack.Screen name="GregList" component={GregListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
