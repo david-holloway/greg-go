3888ed678070bf6555e59d6d614f1fccfa73733e
TechGyal <jadah@vt.edu>
2025-07-25 17:41:29 -0400

    Add SET_CURRENT_USER action and reducer case to manage current user state

--- src/redux/gregUser.js
+++ src/redux/gregUser.js
@@ -1,6 +1,7 @@
 // Action Types
 const ADD_GREG_USER = 'ADD_GREG_USER';
 const SET_GREG_USERS = 'SET_GREG_USERS'; // replacing the entire users array (after fetching from the API).
+const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
 // Action Creators
 export const addGregUser = (user) => ({
@@ -16,6 +17,7 @@ export const setGregUsers = (users) => ({
 // Initial State
 const initialState = {
   users: [], // Array of { uniqueID, location, description, photoFilePath }
+  currentUser: null, // The user currently being edited or viewed
 };
 
 // Reducer - handles actions to update the state
@@ -26,11 +28,19 @@ export default function gregUserReducer(state = initialState, action) {
         ...state,
         users: [...state.users, action.payload],
       };
+
     case SET_GREG_USERS:
       return {
         ...state,
         users: action.payload,
       };
+
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+
     default:
       return state;
   }
