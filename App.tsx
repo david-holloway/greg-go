233563bc1edf91f52a805857bf53ecbd5d785265
TechGyal <jadah@vt.edu>
2025-07-25 17:22:40 -0400

    feat: wrap app with Redux Provider to enable global state management

--- App.tsx
+++ App.tsx
@@ -4,17 +4,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import NameScreen from './src/screens/NameScreen';
 import PhotoScreen from './src/screens/PhotoScreen';
+import { Provider } from 'react-redux';
+import store from './src/redux/store'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Name" component={NameScreen} />
-        <Stack.Screen name="Photo" component={PhotoScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Name" component={NameScreen} />
+          <Stack.Screen name="Photo" component={PhotoScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
